7a1299ba5fbc45959d07c8e58d76cba188e1472a
Chris Pitcher <78032120+cp2423mv@users.noreply.github.com>
2022-09-20 19:43:17 +0100

    Use the request values rather than a URL variable

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "Hello world!"
 
-@app.route('/greet/<person>')
+@app.route('/greet')
 def greet(person):
-  return "Hello " + person
+  return "Hello " + request.args.get('person', '')
